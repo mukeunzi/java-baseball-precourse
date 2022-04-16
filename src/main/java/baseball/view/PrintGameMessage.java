3b3c31b8fa5549904f0da40a6e133a150bbd5fbb
mukeunzi <eunji057@gmail.com>
2022-04-17 03:53:05 +0900

    feat(message): 게임 중 메시지를 출력하는 PrintGameMessage 클래스 생성
    - 게임 시작 시 메시지 출력을 위한 printInputMessage() 메소드 추가

---
+++ src/main/java/baseball/view/PrintGameMessage.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+import baseball.message.GameMessage;
+
+public class PrintGameMessage {
+    public static void printInputMessage() {
+        System.out.print(GameMessage.INPUT_MESSAGE);
+    }
+}
