be574da59d6fbf7cb6979e1f86050351f516a59f
mukeunzi <eunji057@gmail.com>
2022-04-16 17:57:20 +0900

    feat(message): 에러 메시지 출력을 위한 클래스 작성

---
+++ src/main/java/baseball/message/ErrorMessage.java
@@ -0,0 +1,7 @@
+package baseball.message;
+
+public class ErrorMessage {
+    public static final String DIGITS_ERROR = "3자리 숫자만 입력할 수 있습니다.";
+    public static final String TYPE_ERROR = "숫자만 입력할 수 있습니다.";
+    public static final String DUPLICATE_ERROR = "중복되지 않는 숫자만 입력할 수 있습니다.";
+}
