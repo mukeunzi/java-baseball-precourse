86797f5f7ebbcde3b44a54a3bd27219039131d74
mukeunzi <eunji057@gmail.com>
2022-04-17 08:25:29 +0900

    feat(message): 게임 재시작, 완전종료 시에 사용되는 메시지 변수 추가

--- src/main/java/baseball/message/ErrorMessage.java
+++ src/main/java/baseball/message/ErrorMessage.java
@@ -4,4 +4,5 @@ public class ErrorMessage {
     public static final String DIGITS_ERROR = "3자리 숫자만 입력할 수 있습니다.";
     public static final String TYPE_ERROR = "숫자만 입력할 수 있습니다.";
     public static final String DUPLICATE_ERROR = "중복되지 않는 숫자만 입력할 수 있습니다.";
+    public static final String RETRY_ERROR = "1과 2만 입력할 수 있습니다.";
 }
--- src/main/java/baseball/message/GameMessage.java
+++ src/main/java/baseball/message/GameMessage.java
@@ -1,9 +1,12 @@
 package baseball.message;
 
+import baseball.domain.Baseball;
+
 public class GameMessage {
     public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     public static final String BALL_MESSAGE = "볼";
     public static final String STRIKE_MESSAGE = "스트라이크";
-    public static final String NOTTHING_MESSAGE = "낫싱";
-    public static final String FINISH_MESSAGE = "개의 숫자를 모두 맞히셨습니다! 게임 종료" + "\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String NOTHING_MESSAGE = "낫싱";
+    public static final String RETRY_MESSAGE = Baseball.BASEBALL_DIGITS + "개의 숫자를 모두 맞히셨습니다! 게임 종료" + "\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String END_MESSAGE = "게임이 종료되었습니다.";
 }
