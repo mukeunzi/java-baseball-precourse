133d30b62c0f9b940ad72a3a67edbbbbc7824f50
mukeunzi <eunji057@gmail.com>
2022-04-17 01:17:27 +0900

    feat(domain): Baseball 클래스에 input 값 유효성검증 로직 추가
    - validateDigits() - input 값의 자릿수를 검증하는 메소드
    - validateType() - input 값의 타입을 검증하는 메소드
    - validateUnique() - input 값의 중복 여부를 검증하는 메소드
    - validateInput() - 위 3개의 유효성 검증 메소드를 실행하는 역할

--- src/main/java/baseball/domain/Baseball.java
+++ src/main/java/baseball/domain/Baseball.java
@@ -1,6 +1,48 @@
 package baseball.domain;
 
+import baseball.exception.InvalidParameterError;
+import baseball.message.ErrorMessage;
+
+import java.util.HashSet;
+
 public class Baseball {
+    public static final int BASEBALL_DIGITS = 3;
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 9;
+    private int baseballNumbers[];
+
+    public Baseball() {
+        this.baseballNumbers = new int[BASEBALL_DIGITS];
+    }
+
+    public void validateInput(String inputBaseball) {
+        validateDigits(inputBaseball);
+        validateType(inputBaseball);
+        validateUnique(inputBaseball);
+    }
+
+    public void validateDigits(String inputBaseball) {
+        if (inputBaseball.length() != BASEBALL_DIGITS) {
+            throw new InvalidParameterError(ErrorMessage.DIGITS_ERROR);
+        }
+    }
+
+    public void validateType(String inputBaseball) {
+        final String NUMBER_VALID_REGEX = "[1-9]";
+
+        if (!inputBaseball.matches(NUMBER_VALID_REGEX)) {
+            throw new InvalidParameterError(ErrorMessage.TYPE_ERROR);
+        }
+    }
+
+    public void validateUnique(String inputBaseball) {
+        HashSet<String> baseballSet = new HashSet<>();
+        for (String input : inputBaseball.split("")) {
+            baseballSet.add(input);
+        }
+
+        if (baseballSet.size() != BASEBALL_DIGITS) {
+            throw new InvalidParameterError(ErrorMessage.DUPLICATE_ERROR);
+        }
+    }
 }
