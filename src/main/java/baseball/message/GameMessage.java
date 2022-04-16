eb857893fb28a642592463306d466a4da9a62af9
mukeunzi <eunji057@gmail.com>
2022-04-16 18:08:42 +0900

    feat(message): 야구 게임 과정에서 필요한 메시지 클래스 작성

---
+++ src/main/java/baseball/message/GameMessage.java
@@ -0,0 +1,9 @@
+package baseball.message;
+
+public class GameMessage {
+    public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String BALL_MESSAGE = "볼";
+    public static final String STRIKE_MESSAGE = "스트라이크";
+    public static final String NOTTHING_MESSAGE = "낫싱";
+    public static final String FINISH_MESSAGE = "개의 숫자를 모두 맞히셨습니다! 게임 종료" + "\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
